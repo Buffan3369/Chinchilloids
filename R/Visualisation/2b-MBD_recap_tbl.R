8c85dc5c7e42a0c81bdbb3a739a25a4ecc4bbe1f
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2026-05-27 10:37:08 +0200

    MBD tables

--- R/Visualisation/2b-MBD_recap_tbl.R
+++ R/Visualisation/2b-MBD_recap_tbl.R
@@ -9,26 +9,26 @@
 source("./R/useful/MBD_accessory.R")
 source("./R/useful/helper_functions.R")  
 
-rid <- paste0("./Results/MBD/species/MBD_scaled_hsp0/")
+rid <- paste0("./Results/MBD/species_hsp0/")
 # 2.5, 50 and 97.5% quantiles for G coefficients
 G_post <- get_post(rid, param = "G")
 mid_qG <- apply(G_post, MARGIN = 2, FUN = function(x){
   val <- as.numeric(quantile(x, probs = c(0.5)))
-  return(round(val, digits = 3))
+  return(round(val, digits = 2))
 })
 HPD_G <- apply(G_post, MARGIN = 2, FUN = function(x){
   q <- as.numeric(quantile(x, probs = c(0.025, 0.975)))
-  return(paste0("[", round(q[1], digits = 3), "; ", round(q[2], digits = 3), "]"))
+  return(paste0("[", round(q[1], digits = 2), "; ", round(q[2], digits = 2), "]"))
 })
 # 2.5, 50 and 97.5% quantiles for W coefficients
 W_post <- get_post(rid, param = "W")
 mid_qW <- apply(W_post, MARGIN = 2, FUN = function(x){
   val <- as.numeric(quantile(x, probs = c(0.5)))
-  return(round(val, digits = 3))
+  return(round(val, digits = 2))
 })    
 HPD_W <- apply(W_post, MARGIN = 2, FUN = function(x){
   q <- as.numeric(quantile(x, probs = c(0.025, 0.975)))
-  return(paste0("[", round(q[1], digits = 3), "; ", round(q[2], digits = 3), "]"))
+  return(paste0("[", round(q[1], digits = 2), "; ", round(q[2], digits = 2), "]"))
 })
 # define covariable dict
 CovDict <- covar_idx
@@ -45,5 +45,12 @@ merged <- data.frame(Covar = values(CovDict),
                      G_mu_95HPD = HPD_G[b:c],
                      W_G_mu = mid_qW[b:c],
                      W_G_mu_95HPD = HPD_W[b:c])
+rownames(merged) <- 1:nrow(merged)
+
+merged_sp <- merged %>% 
+  select(Covar, G_lamb, G_lamb_95HPD)
+merged_ex <- merged %>% 
+  select(Covar, G_mu, G_mu_95HPD)
 # Save
-write.table.lucas(merged, "./Figures/MBD/MBD_recap_tbl/Species_scaled_hsp0_MBD_recap_tbl.txt")
+saveRDS(merged_sp, "./Data/supp_tbl/MBD_recap_tbl/MBD_speciation_recap_tbl.RDS")
+saveRDS(merged_ex, "./Data/supp_tbl/MBD_recap_tbl/MBD_extinction_recap_tbl.RDS")
